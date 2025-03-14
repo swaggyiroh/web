56966f93d58ce26092a52103f425d4a7aa3871bb
swaggyiroh <dominik@schmitt4.de>
2025-03-14 11:25:01 +0100

    deploy

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import Name from '@/components/Name.vue';
 import Items from '@/components/Items.vue';
 import Timer from '@/components/Timer.vue';
+import Snow from '@/components/Snow.vue'
+
 import { ref, computed, onMounted, onUnmounted } from 'vue';
 
 const bg = ref((Math.random() * 360));
@@ -17,9 +19,17 @@ const dynamicBackground = computed(() => {
   return `linear-gradient(${bg.value}deg, hsl(${bg.value}, 80%, 40%) 0%, hsl(${(bg.value + 180) % 360}, 80%, 20%) 100%)`;
 });
 
+  const year = new Date().getFullYear().toString();
+  const snowy = new Date() > new Date("Dec 1,"+ year || today < new Date("Jan 3,"+ year))
+
 </script>
 
+
+
 <template>
+
+<Snow v-if="snowy"/>
+
   <Timer class="text-xl pl-2" />
   <div 
     class="flex flex-col min-h-screen justify-center items-center transition-colors duration-500"
